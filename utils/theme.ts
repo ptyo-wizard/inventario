770d5043883db3234a729cddd46737cfb2d030c8
ptyo.wizard <ptyo.wizard@gmail.com>
2021-09-12 17:02:45 -0500

    add theme personalizado, color primario y secundario

---
+++ utils/theme.ts
@@ -0,0 +1,17 @@
+import {createTheme } from '@material-ui/core/styles'
+
+
+const theme = createTheme({
+  palette:{
+    primary:{
+      main:"#14213d"
+    },
+    secondary:{
+       main: "#4791db"
+    },
+    
+
+  },
+});
+
+export default theme;
