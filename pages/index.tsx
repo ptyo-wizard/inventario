40c0bc31868d2ec72356a50df34e3fc9069ce94f
ptyo.wizard <ptyo.wizard@gmail.com>
2021-09-12 18:14:06 -0500

    integracion de datos en componente ProductTable, se usa swr para la peticion de datos dentro de pagina index

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,16 +2,21 @@ import type { NextPage } from 'next'
 import Layaut from '../components/layaout'
 import styles from '../styles/Home.module.css'
 import Button from '@material-ui/core/Button'
+import useSWR from 'swr'
+import { fetcher } from '../utils/fetcher'
+import ProductTable from '../components/product_table.tsx'
 
 
 const Home: NextPage = () => {
+  const { data, error } = useSWR(`http://localhost:3000/api/products`, fetcher);    
+  if (error) return <h2>{error}</h2>
+  if (!data) return <h2>No hay datos  {data}</h2>
+
   return (
     <Layaut>
       {
         <div className={styles.container}>
-          <Button variant="contained" color="primary">
-            button
-          </Button>
+          <ProductTable data={data}></ProductTable>
 
        </div>
       }
