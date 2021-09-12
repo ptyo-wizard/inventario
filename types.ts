8bad6f68dc92a93041d8c5d96b02951e37673eb0
ptyo.wizard <ptyo.wizard@gmail.com>
2021-09-12 11:30:25 -0500

    add archivo types.ts, se definio ProductData, SendData

---
+++ types.ts
@@ -0,0 +1,18 @@
+export interface ProductData {
+    _id?:number | string,      
+    code:string,
+    description: string,    
+    input: number,    
+    output: number,    
+    price: number    
+  }
+
+
+  export interface SendData {    
+    code:string,
+    description: string,    
+    input: number,    
+    output: number,    
+    price: number    
+  }
+  
