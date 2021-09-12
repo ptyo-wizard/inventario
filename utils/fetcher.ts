0b18615e04abccd334cbcc79717b5d832f1d2523
ptyo.wizard <ptyo.wizard@gmail.com>
2021-09-12 18:06:18 -0500

    add  funcion para obtener datos, permite atraves del metodo get obtener todos los productos, esta funcion se usa conjuntamente con el hook useSWR

---
+++ utils/fetcher.ts
@@ -0,0 +1,8 @@
+import fetch from 'isomorphic-unfetch';
+
+export const fetcher = async (url:string)=>{
+    const response = await fetch(url);
+    const { data } = await response.json();
+    
+    return data;
+  }
