9c3db242f93154731c01d05a04e91f99d3fd7fcb
ptyo.wizard <ptyo.wizard@gmail.com>
2021-09-14 18:14:56 -0500

    add componente para crear nuevo producto

---
+++ components/new_product/index.tsx
@@ -0,0 +1,128 @@
+import React, { useState } from 'react';
+import { useRouter } from 'next/router'
+import { FormProps, ProductData, SendData } from '../../types'
+import useFetch from '../../hooks/useFetch'
+import Layaut from '../layaout'
+import FormProduct from '../product_form'
+import { Button, Grid } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+import ModalData from '../modal/modal_data'
+import ModalConfirm from '../modal/modal_confirm'
+
+
+
+
+const useStyles = makeStyles((theme) => ({  
+   
+    btnSuccess: {
+        background: "#39993d",
+        color: "#f5f5f5",
+        "&:hover":{
+            background: "#5fda5fb3",
+        }
+    },
+    
+    countLabel: {
+        fontSize: "1rem",
+        color: "#39993d",
+        textAlign: "center",
+        textDecoration: "underline",
+        width: "100%"
+    },
+    
+
+})
+);
+
+const dataTemp: ProductData = {
+    code: '',
+    description: '',
+    input: 0,
+    output: 0,
+    price: 0
+}
+
+export const NewProduct = () => {
+    const [buffer, setBuffer] = useState<SendData[]>([])
+    const { stateFetch, fetchData, resetState } = useFetch();
+    const [modalIsOpen, setModalIsOpen] = useState(false);
+    const [modalConfirm, setModalConfirm] = useState(false);
+    const router = useRouter();
+    const classes = useStyles();
+
+    const addProduct = (data: SendData) => {
+        setBuffer([
+            ...buffer,
+            data
+        ])
+    }
+
+    const dataForm: FormProps = {
+        data: dataTemp,
+        submit: addProduct
+    }
+
+    const handleBuffer = () => {
+        setModalIsOpen(true);
+    }
+
+    const sendAll = () => {
+        //console.log("send all ", buffer)
+        fetchData({ url: `http://localhost:3000/api/products`, method: 'POST', body: buffer });
+        setModalConfirm(true);
+    }
+
+    const handleAccept = () => {
+        if (stateFetch.isSucces) {
+            setModalConfirm(false);
+            router.push('/');
+        } else {
+            resetState();
+            setModalConfirm(false);
+        }
+
+    }
+
+    return (
+        <Layaut>
+            <FormProduct
+                title='Crear producto nuevo'
+                dataForm={dataForm}
+                //fetch={state}
+                count={
+                    buffer.length > 0 && (
+                        <a href="#" onClick={handleBuffer} className={classes.countLabel} >
+                            Hay {buffer.length} productos
+                        </a>
+                    )
+                }
+                labelButton='Crear Producto'
+            >
+                {
+                    buffer.length > 0 && (
+                        <>
+                            <Grid xs={12} sm={12} item>
+                                <Button
+                                    variant="contained"                                    
+                                    onClick={sendAll}
+                                    type="button"
+                                    fullWidth
+                                    className={classes.btnSuccess}
+
+                                >
+                                    Enviar Todo
+                                </Button>
+                            </Grid>
+                        </>
+                    )
+                }
+            </FormProduct>
+
+            <ModalData buffer={buffer} setBuffer={setBuffer} open={modalIsOpen} setOpen={setModalIsOpen} />
+            <ModalConfirm open={modalConfirm} setOpen={setModalConfirm} stateFetch={stateFetch} accept={handleAccept} />
+
+        </Layaut>
+
+    )
+}
+
