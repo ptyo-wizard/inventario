71a1f0e742f03454857a890e8c5566a22bed3a23
ptyo.wizard <ptyo.wizard@gmail.com>
2021-09-14 18:15:41 -0500

    add pagina que permite crear nuevo producto

---
+++ pages/create_product.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { NewProduct } from '../components/new_product'
+
+
+
+export default function CreateProduct  ()  {
+    return (  
+        <div>
+        <NewProduct />
+        
+        </div>     
+    )
+}
