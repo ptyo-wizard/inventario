fe4a9bc528fd7bf70dde8a2a35e2c068803fbf0e
ptyo.wizard <ptyo.wizard@gmail.com>
2021-09-15 13:22:41 -0500

    se ha renombrado archivo [id].ts  a [productId].ts, se hacede al query con ->productId

---
+++ pages/api/products/[productId].ts
@@ -0,0 +1,97 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+import dbConnect from '../../../utils/dbConnect';
+import Inventory from '../../../models/inventory';
+
+interface ProductData {
+    _id?:number | string,      
+    code:string,
+    description: string,    
+    input: number,    
+    output: number,    
+    price: number    
+  }
+  
+  type ResponseData = {
+    success:boolean,
+    data?:ProductData | {}
+  }
+  
+
+dbConnect();
+
+export default async function handler(
+    req: NextApiRequest, 
+    res: NextApiResponse <ResponseData>
+    ){
+        const {
+            query: {  productId },
+            method,
+          } = req;
+        
+          switch (method) {
+            case 'GET':
+              try {
+                const productList = await Inventory.findById(productId);
+                if (!productList) {
+                  return res.status(400).json({
+                    success: false,
+                  });
+                }
+                res.status(200).json({
+                  success: true,
+                  data: productList,
+                });
+              } catch (error) {
+                res.status(400).json({
+                  success: false,
+                });
+              }
+              break;
+            case 'PUT':
+              try {
+                const updateProduct = await Inventory.findByIdAndUpdate(productId, req.body, {
+                  new: true,
+                  runValidators: true,
+                });
+                if (!updateProduct) {
+                  return res.status(400).json({
+                    success: false,
+                  });
+                }
+                res.status(200).json({
+                  success: true,
+                  data: updateProduct,
+                });
+              } catch (error) {
+                res.status(400).json({
+                  success: false,
+                });
+              }
+              break;
+            case 'DELETE':
+              try {
+                const delProduct = await Inventory.deleteOne({ _id: productId });
+                //console.log("********** ", delProduct)
+
+                if (delProduct.deletedCount === 0) {
+                  return res.status(400).json({
+                    success: false,
+                  });
+                }
+
+                res.status(200).json({
+                  success: true,
+                  data: {},
+                });
+              } catch (error) {
+                res.status(401).json({
+                  success: false,
+                });
+              }
+              break;
+            default:
+              res.status(500).json({
+                success: false,
+              });
+          }
+};
