170a6a77f32aaad1c1f6cec3f36dde64a8c304cc
ptyo.wizard <ptyo.wizard@gmail.com>
2021-09-15 19:46:51 -0500

    add componente modal_confirm, permite mostrar un modal para confirmar si se quiere eliminar un elemento

--- components/modal/modal_confirm.tsx
+++ components/modal/modal_confirm.tsx
@@ -1,150 +1,120 @@
 
 import React from 'react';
 import Modal from '@material-ui/core/Modal'
-import { FetchProps} from '../../types'
+import { FetchProps } from '../../types'
 import { Button, Typography } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
 
 
-const useStyles = makeStyles((theme)=>({
-    modal:{
-        display:"flex",
-        justifyContent:"center",
-        alignItems:"center",
+const useStyles = makeStyles((theme) => ({
+    modal: {
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
     },
-    modalBtn:{
-        display: "grid",    
-        gap: "10px",
-        gridAautoFlow: "dense",
-        gridAautoEows: "40px",
-        gridTemplateColumns: "repeat(auto-fit,minmax(min(100%,120px),1fr))",
-        placeItems:"center",
-        marginTop: "10px"
-    },
-    
-    confirmModal:{
+    modalBtn: {
         display:"flex",
-        justifyContent:"center",
+        justifyContent:"space-between",
         alignItems:"center",
+        width:"100%",
+        marginTop:"1rem"
         
-        width:"250px",
-        height:"130px",
-        background:"white",
-       
-        boxShadow:theme.shadows[5],
-        borderRadius:"5px"
     },
-    btnSuccess:{
+
+    confirmModal: {
+        display: "flex",
+        flexDirection:"column",
+        justifyContent: "center",
+        alignItems: "center",
+
+        width: "250px",
+        height: "130px",
+        background: "white",
+
+        boxShadow: theme.shadows[5],
+        borderRadius: "5px",
+        padding:"1rem"
+        
+    },
+    btnSuccess: {
         background: "#39993d",
-        color:"#f5f5f5",
-        "&:hover":{
+        color: "#f5f5f5",
+        "&:hover": {
             background: "#5fda5fb3",
         }
     },
-    btnError:{
+    btnError: {
         background: "#d63434",
-        color:"#f5f5f5",
-        "&:hover":{
+        color: "#f5f5f5",
+        "&:hover": {
             background: "#db4c4c",
         }
     },
-    textError:{
-        fontSize:"1rem",
+    textError: {
+        fontSize: "1rem",
         color: "#d63434",
     },
-    textSuccess:{
-        fontSize:"1rem",
-        color: "#39993d",        
+    textSuccess: {
+        fontSize: "1rem",
+        color: "#39993d",
     },
-  
-    textLoading:{
-        fontSize:"1rem",
-        color:"#2b2bb1"
-    }
 
+   
 })
 );
 
-type ModalConfirmProps = {    
+type ModalConfirmProps = {
     open: boolean,
-    setOpen:(isOpen:boolean)=>void,
-    stateFetch:FetchProps,
-    accept:()=>void
-    messages?:{
-        loading:string,
-        success:string,
-        failed:string
-    }
+    setOpen: (isOpen: boolean) => void,
+    accept: (confirm:boolean) => void,    
+    messages?: string
 }
 
 
 
-const ModalConfirm = ({open,setOpen,stateFetch,accept,messages}:ModalConfirmProps) => {
+const ModalConfirm = ({ open, setOpen, accept,  messages }: ModalConfirmProps) => {
     const classes = useStyles();
 
     const bodyConfirmModal = (
-        <div className={classes.confirmModal}>
-                   
-        {
-                stateFetch.isLoading && 
-                <Typography className={classes.textLoading}>
-                    {messages?.loading}
+        <div className={classes.confirmModal}>            
+                <Typography className={classes.textError} align="center">
+                    {messages}
                 </Typography>
-                
-        }
-        {
-                stateFetch.isSucces && !stateFetch.isFailed  &&(
-                    <div>
-                        <Typography className={classes.textSuccess}>
-                            {messages?.success}
-                        </Typography>
-                        <div className={classes.modalBtn}>
-                            <Button 
-                                variant="contained" 
-                                onClick={accept} 
-                                type="button"  
-                                className={classes.btnSuccess}
-                            >
-                              Aceptar
-                            </Button>
-                            
-                        </div>                       
+                <div className={classes.modalBtn}>
+                    <Button
+                        variant="contained"
+                        onClick={()=>accept(true)}
+                        type="button"
+                        className={classes.btnSuccess}
+                        size="small"
+                    >
+                        Aceptar
+                    </Button>
+                    
+                    <Button
+                        variant="contained"
+                        type="button"
+                        onClick={()=>setOpen(false)}
+                        className={classes.btnError}
+                        size="small"
+                    >
+                        Cancelar
+                    </Button>             
 
-                    </div>
-
-                )
-            }
-            {
-                stateFetch.isFailed && (
-                    <div>
-                        <Typography className={classes.textError}>
-                            {messages?.failed}
-                        </Typography>
-                        <div className={classes.modalBtn}>
-                            <Button 
-                                variant="contained"                               
-                                type="button"
-                                onClick={accept} 
-                                className={classes.btnError}
-                            >
-                                Aceptar
-                            </Button>                            
-                        </div>
-                    </div>
-                )
-            }
+                </div>            
         </div>
     )
+
     return (
         <Modal
-        open={open}
-        onClose={setOpen}
-        aria-labelledby="simple-modal-title"
-        aria-describedby="simple-modal-description"                
-        className={classes.modal}
-    >
-        {bodyConfirmModal}               
-    </Modal> 
+            open={open}
+            onClose={setOpen}
+            aria-labelledby="simple-modal-title"
+            aria-describedby="simple-modal-description"
+            className={classes.modal}
+        >
+            {bodyConfirmModal}
+        </Modal>
     )
 }
 
