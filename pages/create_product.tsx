f80953bd6152fa36ce04d630942c7c7e1ba4dca7
ptyo.wizard <ptyo.wizard@gmail.com>
2021-09-20 09:54:09 -0500

    add panel a pagina create_product, habilitado boton nueva entrada

--- pages/create_product.tsx
+++ pages/create_product.tsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import { NewProduct } from '../components/new_product'
+import Panel from '../components/new_product_panel'
 
 
 
 export default function CreateProduct  ()  {
     return (  
         <div>
-        <NewProduct />
+        {/* <NewProduct /> */}
+        <Panel></Panel>
         
         </div>     
     )
